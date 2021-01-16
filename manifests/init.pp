fba85d4e47fecc536bea83f84d0327175cd852ed
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-16 15:01:08 +0100

    initial implementation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,27 @@
 #
-class profile_template (
+#
+#
+class profile_rsnapshot (
+  String               $collect_tag,
+  Hash                 $intervals,
+  Hash                 $timers,
+  Stdlib::AbsolutePath $mount_dir,
+  String               $data_device,
 ) {
+  profile_base::mount{ $mount_dir:
+    device => $data_device,
+    before => Class['rsnapshot'],
+  }
+
+  class { 'rsnapshot':
+    intervals => $intervals,
+    timers    => $timers,
+  }
+
+  #include profile_rsnapshot::webserver
+
+  # Collect exported resources
+  Rsnapshot::Backup <<| tag == $collect_tag |>>
+  Rsnapshot::Backup_script <<| tag == $collect_tag |>>
+  Sshkey <<| tag == $collect_tag |>>
 }
