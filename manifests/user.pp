cb14db5febcfe3da548b466aed8bb4c66b8f4837
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-16 21:20:22 +0100

    add autorised_keys lookup

--- manifests/user.pp
+++ manifests/user.pp
@@ -3,6 +3,7 @@
 #
 class profile_rsnapshot::user {
   class { 'rsnapshot::user':
-    tag => lookup('rsnapshot_tag', String, undef, 'rsnapshot')
+    tag             => lookup('rsnapshot_tag', String, undef, 'rsnapshot'),
+    authorized_keys => lookup('rsnapshot_authorized_keys', Hash, undef, {}),
   }
 }
